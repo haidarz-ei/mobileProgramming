1f2e04fb2926c5b273cba58d3f13e69ea9075b99
haidarz-ei <zzhaidar36@gmail.com>
2025-09-25 21:40:03 +0700

    5

--- lib/tugas/quiz.dart
+++ lib/tugas/quiz.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/latihan/stack.dart';
 
 class quiz extends StatelessWidget {
   const quiz({super.key});
@@ -7,7 +8,8 @@ class quiz extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Training"),
+          title: 
+          Text("Training"),
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(vertical: 30),
@@ -23,8 +25,8 @@ class quiz extends StatelessWidget {
                   Container(
                     child: Row(
                       children: [
-                        Text("Detail", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                        Icon(Icons.arrow_forward_ios_outlined),
+                        Text("Detail", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.blueAccent)),
+                        Icon(Icons.arrow_forward_ios_outlined, color: Colors.blue,),
                       ],
                     )
                   )
@@ -34,24 +36,38 @@ class quiz extends StatelessWidget {
             
             SizedBox(height: 15),
 
-            Container(
-              padding: EdgeInsets.symmetric(vertical: 40),
-              width: 400,
-              height: 200,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(15)),
-                  gradient: LinearGradient(
-                    colors: [Colors.purple, Colors.white],
-                    begin: Alignment.centerLeft,
-                    end: Alignment.centerRight,
-                    stops: [0.2, 1],
-                  )
-              ),
-                child: Row(
-                  children: [
-                    
-                  ],
+            Positioned(
+              top: 235,
+              
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 40),
+                width: 400,
+                height: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                    gradient: LinearGradient(
+                      colors: [Colors.purple, Colors.white],
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      stops: [0.2, 1],
+                    )
                 ),
+                  child: Column(
+                    children: [
+                      Text("Next Workout", style: TextStyle(fontSize: 15, color: Colors.white)),
+                      Text("Lets Toning \nand Glutes Workout", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.white)),
+                      Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("60 min", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.white)),
+                            SizedBox(width: 300),
+                            Icon(Icons.arrow_forward_ios_outlined)
+                          ],
+                        ))
+                    ],
+                  ),
+              ),
             ),
 
             SizedBox(height: 20),
@@ -65,9 +81,10 @@ class quiz extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
 
-                Container(
+                Align(
+                  alignment: Alignment(-0.7,1),
                   child: 
-                    Text("Area of focus")
+                    Text("Area of focus", style: TextStyle(fontSize: 20),)
                 ),
 
                 
@@ -82,6 +99,21 @@ class quiz extends StatelessWidget {
                             borderRadius: BorderRadius.all(Radius.circular(20)),
                             color: Colors.blue
                           ),
+                          child: Stack(
+                            children: [
+                              Align(
+                                alignment: Alignment(0,0.5),
+                                child: Container(
+                                  width: 250,
+                                  height: 280,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(image: AssetImage("assets/img/ex3.png")
+                                    )
+                                  ),
+                                ),
+                              ),
+                            ]
+                          ),
                         ),
                         SizedBox(width: 10,),
                         Container(
@@ -90,6 +122,21 @@ class quiz extends StatelessWidget {
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.all(Radius.circular(20)),
                             color: Colors.blue
+                          ),
+                          child: Stack(
+                            children: [
+                              Align(
+                                alignment: Alignment(0,0.5),
+                                child: Container(
+                                  width: 250,
+                                  height: 280,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(image: AssetImage("assets/img/ex4.png")
+                                    )
+                                  ),
+                                ),
+                              ),
+                            ]
                           ),
                         )
                       ],

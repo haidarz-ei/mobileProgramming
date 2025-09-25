7210cc1bd64ae90fb7794eb343f2f5af01ba44bd
haidarz-ei <zzhaidar36@gmail.com>
2025-09-25 20:54:33 +0700

    every 15s 3

--- lib/tugas/quiz.dart
+++ lib/tugas/quiz.dart
@@ -7,7 +7,7 @@ class quiz extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-        title: Text("Training"),
+          title: Text("Training"),
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(vertical: 30),
@@ -49,186 +49,32 @@ class quiz extends StatelessWidget {
 
             SizedBox(height: 15),
 
+            Image(image: AssetImage("assets/img/card.jpg")),
+
             Container(
-              padding: EdgeInsets.symmetric(vertical: 40),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(50)),
-                image: DecorationImage(image: AssetImage("assets/img/card.jpg"))
-                ),
+              child: 
+                Text("Area of focus")
             ),
 
             Container(
-              padding: EdgeInsets.symmetric(vertical: 40),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-
-                    Container(
-                      padding: EdgeInsets.symmetric(vertical: 18, horizontal: 25),
-                      height: 240,
-                      width: 220,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(15)),
-                        color: const Color.fromARGB(255, 85, 105, 255),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Icon(Icons.stacked_line_chart, color: Colors.white, size: 30),
-
-                          SizedBox(height: 15),
-
-                          Text("Earnings", style: TextStyle(fontSize: 20, color: Colors.white)),
-
-                          SizedBox(height: 15),
-
-                          Container(
-                            child: Row(
-                              children: [
-                                Text("\$", style: TextStyle(fontSize: 50, color: const Color.fromARGB(255, 195, 206, 255), fontFamily: 'ArialNarrow')),
-                                Text("8,350", style: TextStyle(fontSize: 50, color: Colors.white))
-                              ],
-                            )
-                          ),
-
-                          Container(
-                            padding: EdgeInsets.symmetric(horizontal: 9, vertical: 5),
-                            height: 30,
-                            width: 160,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.all(Radius.circular(15)),
-                              color: const Color.fromARGB(255, 79, 97, 236),
-                            ),
-                            child: Text("+ 10% since last month", style: TextStyle(fontSize: 13, color: Colors.white))),
-                        ],
-                      ),
+              child: Row(
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(0)),
+                      color: Colors.blue
                     ),
+                  ),
+                  Container(
 
-                    SizedBox(width: 20),
+                  )
+                ],
+              ),
+            )
 
-                    Container(
-                      child: Column(
-                        children: [
 
-                          Container(
-                            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-                            height: 90,
-                            width: 220,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.all(Radius.circular(15)),
-                              color: const Color.fromARGB(255, 236, 235, 255),
-                            ),
-                            child: Row(
-                              
-                              children: [
-                                Container(
-                                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
-                                  width: 50,
-                                  height: 50,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                                    color: const Color.fromARGB(255, 85, 105, 255),
-                                  ),
-                                  child: Text("98", style: TextStyle(fontSize: 25, color: Colors.white)),
-                                ),
-
-                                SizedBox(width: 10),
-
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text("Rank", style: TextStyle(fontSize: 15, color: Colors.black, fontWeight: FontWeight.bold)),
-                                    Text("In top 30%"),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-
-                          SizedBox(height: 20),
-
-                          Container(
-                            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-                            height: 130,
-                            width: 220,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.all(Radius.circular(15)),
-                              color: const Color.fromARGB(255, 236, 235, 255),
-                            ),
-                            child: Column(
-                              children: [
-
-                                Container(
-                                  child: Row(
-                                    children: [
-                                      Container(
-                                        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
-                                        width: 50,
-                                        height: 50, 
-                                        decoration: BoxDecoration(
-                                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                                          color: const Color.fromARGB(255, 85, 105, 255),
-                                        ),
-                                        child: Text("32", style: TextStyle(fontSize: 25, color: Colors.white)),
-                                      ),
-
-                                      SizedBox(width: 10),
-
-                                      Column(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: [
-                                          Text("Projects", style: TextStyle(fontSize: 15, color: Colors.black, fontWeight: FontWeight.bold)),
-                                          Text("8 this month"),
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                ),
-
-                                SizedBox(height: 10),
-
-                                Container(
-                                  child: Row(
-                                    children: [
-                                      Container(
-                                        padding: EdgeInsets.symmetric(vertical: 5, horizontal: 11),
-                                        height: 30,
-                                        width: 95,
-                                        decoration: BoxDecoration(
-                                          borderRadius: BorderRadius.all(Radius.circular(15)),
-                                          color: const Color.fromARGB(255, 221, 220, 255),
-                                        ),
-                                        child: Text("mobile app")
-                                      ),
-
-                                      SizedBox(width: 5),
-
-                                      Container(
-                                        padding: EdgeInsets.symmetric(vertical: 5, horizontal: 11),
-                                        height: 30,
-                                        width: 80,
-                                        decoration: BoxDecoration(
-                                          borderRadius: BorderRadius.all(Radius.circular(15)),
-                                          color: const Color.fromARGB(255, 221, 220, 255),
-                                        ),
-                                        child: Text("branding")),
-                                    ],
-                                  ),
-                                ),
-
-                              ],
-                            ),
-                          ),
-                    
-                        ],
-                      )
-                    )
-
-                  ],
-                ),
-            ),
             
           ],
         ),
